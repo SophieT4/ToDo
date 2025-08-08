b09d7b4988231ddcd62af5f335a4e823fa0d5151
Sophie <thanjusophie@gmail.com>
2025-08-08 13:09:15 -0400

    Set up the To-Do List title, + button, and made new view

---
+++ ToDo/NewToDoView.swift
@@ -0,0 +1,20 @@
+//
+//  NewToDoView.swift
+//  ToDo
+//
+//  Created by Scholar on 8/8/25.
+//
+
+import SwiftUI
+
+struct NewToDoView: View {
+    var body: some View {
+        VStack {
+            Text("Task title:")
+        }
+    }
+}
+
+#Preview {
+    NewToDoView()
+}
